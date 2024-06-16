4f89e0900a019ee32fd51c80d6c014e5d53f6eb6
Sharllack <164765027+Sharllack@users.noreply.github.com>
2024-06-16 19:05:10 -0300

    Adicionando API para animações

---
+++ JS/animacao.js
@@ -0,0 +1,34 @@
+window.sr = ScrollReveal({ reset: true })
+
+
+ScrollReveal({ distance: '1000px' })
+ScrollReveal().reveal('.borderHistoria', {
+    origin: 'left',
+    delay: 200,
+    
+});
+ScrollReveal().reveal('.borderPerfil', { 
+    origin: 'right',
+    delay: 200,
+    
+});
+ScrollReveal().reveal('.topicos', {
+    origin: 'left',
+    delay: 200,
+    
+});
+ScrollReveal().reveal('.borderArea', {
+    origin: 'left',
+    delay: 200,
+    
+});
+ScrollReveal().reveal('.borderAreas', { 
+    origin: 'right',
+    delay: 200,
+    
+});
+ScrollReveal().reveal('.redes', { 
+    origin: 'right',
+    delay: 200,
+    
+});
