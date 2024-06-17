950d559ed92c3ca906868a9b49985a8b6d343dbb
Sharllack <164765027+Sharllack@users.noreply.github.com>
2024-06-16 23:01:20 -0300

    Mudando cores e posições

--- JS/animacao.js
+++ JS/animacao.js
@@ -4,31 +4,31 @@ window.sr = ScrollReveal({ reset: true })
 ScrollReveal({ distance: '1000px' })
 ScrollReveal().reveal('.borderHistoria', {
     origin: 'left',
-    delay: 200,
+    delay: 100,
     
 });
 ScrollReveal().reveal('.borderPerfil', { 
     origin: 'right',
-    delay: 200,
+    delay: 100,
     
 });
 ScrollReveal().reveal('.topicos', {
     origin: 'left',
-    delay: 200,
+    delay: 100,
     
 });
 ScrollReveal().reveal('.borderArea', {
     origin: 'left',
-    delay: 200,
+    delay: 100,
     
 });
 ScrollReveal().reveal('.borderAreas', { 
     origin: 'right',
-    delay: 200,
+    delay: 100,
     
 });
 ScrollReveal().reveal('.redes', { 
     origin: 'right',
-    delay: 200,
+    delay: 100,
     
 });
